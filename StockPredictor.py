ceb19a4bcf41abb466eeb498fc04d78f9ea37099
Maheel Dabarera <maheel.m6@gmail.com>
2023-07-26 22:44:01 +0200

    New Algorithm

--- StockPredictor.py
+++ StockPredictor.py
@@ -1,20 +1,87 @@
+import numpy as np
 import pandas as pd
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+from flask import Flask, render_template, request, jsonify
+from alpha_vantage.timeseries import TimeSeries
+import tensorflow as tf  # Import TensorFlow for RNN model
 
-class StockPredictor:
-    def __init__(self, data):
-        self.data = data
-        self.regressor = self._train_model()
+app = Flask(__name__)
 
-    def _train_model(self):
-        X = self.data[['Time']]
-        y = self.data['StockPrice']
-        kernel = 1.0 * RBF()
-        regressor = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
-        regressor.fit(X, y)
-        return regressor
+# Function to fetch real-time stock data using Alpha Vantage API
+def fetch_stock_data(symbol):
+    # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key (https://www.alphavantage.co/support/#api-key)
+    api_key = 'G3WDE6TMLADW34S2'
+
+    # Initialize the Alpha Vantage API client
+    ts = TimeSeries(key=api_key, output_format='pandas')
+
+    # Get the data, returns a tuple
+    # The first element is a pandas dataframe, and the second element is a dict
+    # Fetch the historical stock data
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='compact')
+
+
+    return data
+
+# Sample stock symbols (replace this with the symbols you want to predict)
+stock_symbols = ['MSFT', 'AAPL', 'NVDA', 'META', 'NFLX', 'TSLA']
+
+# Fetch real-time stock data for the given symbols
+stock_data = {symbol: fetch_stock_data(symbol) for symbol in stock_symbols}
+
+# Placeholder: Load and preprocess your stock data here
+# You can use the 'stock_data' dictionary to access the fetched stock data for each symbol
+# Replace the following placeholder with your data preprocessing code
+
+# Placeholder: Define and train your RNN model here
+# You need to fill out this part based on your specific RNN model architecture and training process
+# Replace the following placeholder with your RNN model definition and training code
+
+# Placeholder: Create an instance of the trained RNN model
+# You need to fill out this part after training your RNN model
+def create_rnn_model():
+    model = tf.keras.Sequential([
+        # Add RNN layers and other necessary layers here
+    ])
+    return model
+
+# Placeholder: Create an instance of the trained RNN model
+# You need to fill out this part after training your RNN model
+rnn_model = create_rnn_model()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        time = float(request.form['time'])
+
+        # Placeholder: Use the trained RNN model to predict stock price
+        predicted_price = predict_stock_price(time)
+
+        return render_template('index.html', time=time, predicted_price=predicted_price)
+    return render_template('index.html')
+
+def predict_stock_price(time):
+    # Placeholder: Use the trained RNN model to predict stock price
+    # You need to replace this placeholder with actual prediction code
+    # For example, if your RNN model is 'rnn_model', you can do something like:
+    # predicted_price = rnn_model.predict(np.array([[time]]))[0][0]
+
+    # Remove the following line after implementing the prediction
+    predicted_price = np.random.randint(100, 200)
 
     def predict_stock_price(self, time):
         predicted_price = self.regressor.predict([[time]])[0]
-        return predicted_price
+    return predicted_price
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.get_json()
+        time = float(data['time'])
+
+        # Placeholder: Use the trained RNN model to predict stock price
+        predicted_price = predict_stock_price(time)
+
+        return jsonify({'predicted_price': predicted_price})
+
+if __name__ == '__main__':
+    app.run(debug=True)
