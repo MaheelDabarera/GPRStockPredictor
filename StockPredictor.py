bb6721c562ef40faaab1b1f0c0d48d16091bfcef
Maheel Dabarera <maheel.m6@gmail.com>
2023-07-27 23:24:18 +0200

    Updated with yfinance

--- StockPredictor.py
+++ StockPredictor.py
@@ -1,87 +1,67 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template, request, jsonify
-from alpha_vantage.timeseries import TimeSeries
-import tensorflow as tf  # Import TensorFlow for RNN model
+import tensorflow as tf
+import yfinance as yf
+from datetime import datetime, timedelta
 
-app = Flask(__name__)
+# This function fetches stock data for all S&P 500 stocks from Yahoo Finance.
+def fetch_sp500_stock_data():
+    sp500_tickers = yf.Tickers('^GSPC').tickers
+    stock_data = {}
+    for ticker in sp500_tickers:
+        data = ticker.history(period='20y', interval='1d')
+        stock_data[ticker.ticker] = data
+    return stock_data
 
-# Function to fetch real-time stock data using Alpha Vantage API
-def fetch_stock_data(symbol):
-    # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key (https://www.alphavantage.co/support/#api-key)
-    api_key = 'G3WDE6TMLADW34S2'
-
-    # Initialize the Alpha Vantage API client
-    ts = TimeSeries(key=api_key, output_format='pandas')
-
-    # Get the data, returns a tuple
-    # The first element is a pandas dataframe, and the second element is a dict
-    # Fetch the historical stock data
-    data, meta_data = ts.get_daily(symbol=symbol, outputsize='compact')
-
-
-    return data
-
-# Sample stock symbols (replace this with the symbols you want to predict)
-stock_symbols = ['MSFT', 'AAPL', 'NVDA', 'META', 'NFLX', 'TSLA']
-
-# Fetch real-time stock data for the given symbols
-stock_data = {symbol: fetch_stock_data(symbol) for symbol in stock_symbols}
-
-# Placeholder: Load and preprocess your stock data here
-# You can use the 'stock_data' dictionary to access the fetched stock data for each symbol
-# Replace the following placeholder with your data preprocessing code
-
-# Placeholder: Define and train your RNN model here
-# You need to fill out this part based on your specific RNN model architecture and training process
-# Replace the following placeholder with your RNN model definition and training code
-
-# Placeholder: Create an instance of the trained RNN model
-# You need to fill out this part after training your RNN model
+# This function creates a simple RNN model.
 def create_rnn_model():
     model = tf.keras.Sequential([
-        # Add RNN layers and other necessary layers here
+        tf.keras.layers.LSTM(64, input_shape=(1, 1)),
+        tf.keras.layers.Dense(1),
     ])
+    model.compile(optimizer='rmsprop', loss='mse')
     return model
 
-# Placeholder: Create an instance of the trained RNN model
-# You need to fill out this part after training your RNN model
-rnn_model = create_rnn_model()
+# This function predicts stock prices for tomorrow using a trained RNN model.
+def predict_stock_prices(time, model, stock_data):
+    predicted_prices = {}
+   
+    for ticker, data in stock_data.items():
+        data = data.drop(['Date', 'Dividends', 'Stock Splits'], axis=1)
+        data = data.dropna()
+        data = data.values.reshape(-1, 1)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        time = float(request.form['time'])
+        price = model.predict(np.array([[time]]))[0][0]
+        predicted_prices[ticker] = price
 
-        # Placeholder: Use the trained RNN model to predict stock price
-        predicted_price = predict_stock_price(time)
+    return predicted_prices
 
-        return render_template('index.html', time=time, predicted_price=predicted_price)
-    return render_template('index.html')
+# This is the main function of the program. It first fetches stock data, then predicts stock prices for tomorrow, and finally prints the predicted prices.
+def main():
 
-def predict_stock_price(time):
-    # Placeholder: Use the trained RNN model to predict stock price
-    # You need to replace this placeholder with actual prediction code
-    # For example, if your RNN model is 'rnn_model', you can do something like:
-    # predicted_price = rnn_model.predict(np.array([[time]]))[0][0]
+    # Disable eager execution for TensorFlow to avoid immediate evaluation of operations.
+    tf.compat.v1.disable_eager_execution()
 
-    # Remove the following line after implementing the prediction
-    predicted_price = np.random.randint(100, 200)
+    # Get the current date.
+    current_date = datetime.now().date()
 
-    def predict_stock_price(self, time):
-        predicted_price = self.regressor.predict([[time]])[0]
-    return predicted_price
+    # Predict stock prices for tomorrow.
+    tomorrow = current_date + timedelta(days=1)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        data = request.get_json()
-        time = float(data['time'])
+    # Fetch stock data for all S&P 500 stocks.
+    stock_data = fetch_sp500_stock_data()
 
-        # Placeholder: Use the trained RNN model to predict stock price
-        predicted_price = predict_stock_price(time)
+    # Create a RNN model.
+    model = create_rnn_model()
 
-        return jsonify({'predicted_price': predicted_price})
+    # Placeholder: Train your RNN model using the stock data
+
+    # Predict stock prices for all stocks in the S&P 500 index.
+    predicted_prices = predict_stock_prices(tomorrow, model, stock_data)
+
+    # Print the predicted stock prices for all stocks.
+    for ticker, price in predicted_prices.items():
+        print(f"Predicted price for {ticker} on {tomorrow}: ${price:.2f}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
